9cb9e75825030fb13eaa7d73c01ee1932ac2ee2a
Aegis864-dein <baderemil@icloud.com>
2025-03-11 18:02:38 +0100

    Update Grafik.java
    :(

--- Grafik.java
+++ Grafik.java
@@ -17,13 +17,14 @@ public class Grafik extends Ereignisanwendung
     Knopf reload2;
     Knopf write;
     Knopf read;
+    Etikett Info;
     double[][] BubbleSave;
     double[][] MergeSave;
     double[][] InsertSave;
     int stepos;
 
     // Konstruktor
-    public Grafik()
+    public Grafik(int durchläufe)
     {
         pen = new Buntstift();
         bildschirm().setSize(2000, 1000);
@@ -35,10 +36,11 @@ public class Grafik extends Ereignisanwendung
         reload2.setzeBearbeiterGeklickt("Reload_Mit_Messergebnisse");
         write.setzeBearbeiterGeklickt("Write_Array");
         read.setzeBearbeiterGeklickt("Read_Array");
+        Info = new Etikett(1310,10,200,100,"Infos");
 
-        BubbleSave = new double[1200][1200];
-        MergeSave = new double[1200][1200];
-        InsertSave = new double[1200][1200];
+        BubbleSave = new double[1200][20000];
+        MergeSave = new double[1200][20000];
+        InsertSave = new double[1200][20000];
 
         BubbleSave[0][0] = 0;
         MergeSave[0][0] = 0;
@@ -78,39 +80,36 @@ public class Grafik extends Ereignisanwendung
         pen.dreheBis(0);
     }
 
-    public void do_Test(int x, double y, int algo, int steps,int current,int speed)
+    public void Kurve_Zeichnen_Iterativ(int x, double y, int algo, int steps,int current,int speed)
     {
         stepos = steps;
         switch(algo)
         {
-                case(1):
+                case(1):               
                 BubbleSave = Average(x,stepos,y,1,current,speed);
-                pen.setzeFarbe(0);
-                pen.bewegeBis(100+(x-stepos),-BubbleSave[x-stepos][0]+810);
-                pen.runter();
-                pen.bewegeBis(100+x,-BubbleSave[x][0]+810);
-                pen.hoch();
+                Datenpunkt_Zeichnen(BubbleSave,x,0);
                 break;
                 case(2):
 
-                pen.setzeFarbe(1);
                 InsertSave = Average(x,stepos,y,2,current,speed);
-                pen.bewegeBis(100+(x-stepos),-InsertSave[x-stepos][0]+810);
-                pen.runter();
-                pen.bewegeBis(100+x,-InsertSave[x][0]+810);
-                pen.hoch();
+                Datenpunkt_Zeichnen(InsertSave,x,1);
                 break;
                 case(3):
 
-                pen.setzeFarbe(7);
                 MergeSave = Average(x,stepos,y,3,current,speed);
-                pen.bewegeBis(100+(x-stepos),-MergeSave[x-stepos][0]+810);
-                pen.runter();
-                pen.bewegeBis(100+x,-MergeSave[x][0]+810);
-                pen.hoch();
+                Datenpunkt_Zeichnen(MergeSave,x,7);
                 break;
         }
 
+    }
+
+    public void Datenpunkt_Zeichnen(double[][] Array,int x,int Farbe)
+    {
+        pen.setzeFarbe(Farbe);
+        pen.bewegeBis(100+(x-stepos),-Array[x-stepos][0]+810);
+        pen.runter();
+        pen.bewegeBis(100+x,-Array[x][0]+810);
+        pen.hoch();
     }
 
     public double[][] Average(int x, int steps, double y, int algo, int current, int speed) {
@@ -140,34 +139,22 @@ public class Grafik extends Ereignisanwendung
         return saveArray; // Das aktualisierte Array zurückgeben
     }
 
-    public void Do(int x,double y, int algo,int steps)
+    public void Kurve_Zeichnen(int x,double y, int algo,int steps)
     {
         stepos = steps;
         switch(algo)
         {
                 case(1):
                 BubbleSave[x][0] = y;
-                pen.setzeFarbe(0);
-                pen.bewegeBis(100+(x-stepos),-BubbleSave[x-stepos][0]+810);
-                pen.runter();
-                pen.bewegeBis(100+x,-y+810);
-                pen.hoch();
+                Datenpunkt_Zeichnen(BubbleSave,x,0);
                 break;
                 case(2):
                 InsertSave[x][0] = y;
-                pen.setzeFarbe(1);
-                pen.bewegeBis(100+(x-stepos),-InsertSave[x-stepos][0]+810);
-                pen.runter();
-                pen.bewegeBis(100+x,-y+810);
-                pen.hoch();
+                Datenpunkt_Zeichnen(InsertSave,x,1);
                 break;
                 case(3):
                 MergeSave[x][0] = y;
-                pen.setzeFarbe(2);
-                pen.bewegeBis(100+(x-stepos),-MergeSave[x-stepos][0]+810);
-                pen.runter();
-                pen.bewegeBis(100+x,-y+810);
-                pen.hoch();
+                Datenpunkt_Zeichnen(MergeSave,x,7);
                 break;
         }
 
@@ -177,17 +164,17 @@ public class Grafik extends Ereignisanwendung
     {
         for(int i = 1; i < 1200; i++)
         {
-            if(BubbleSave[i][0] != 0)Do(i,BubbleSave[i][0],1,stepos);
+            if(BubbleSave[i][0] != 0)Kurve_Zeichnen(i,BubbleSave[i][0],1,stepos);
         }
 
         for(int i = 1; i < 1200; i++)
         {
-            if(InsertSave[i][0] != 0)Do(i,InsertSave[i][0],2,stepos);
+            if(InsertSave[i][0] != 0)Kurve_Zeichnen(i,InsertSave[i][0],2,stepos);
         }
 
         for(int i = 1; i < 1200; i++)
         {
-            if(MergeSave[i][0] != 0)Do(i,MergeSave[i][0],3,stepos);
+            if(MergeSave[i][0] != 0)Kurve_Zeichnen(i,MergeSave[i][0],3,stepos);
         }
     }
 
@@ -199,45 +186,22 @@ public class Grafik extends Ereignisanwendung
 
     public void Write_Array()
     {
+        In_Datei_Schreiben(BubbleSave, "BubbleSort.txt");
+        In_Datei_Schreiben(InsertSave, "InsertSort.txt");
+        In_Datei_Schreiben(MergeSave, "MergeSort.txt");
+    }
+
+    public void In_Datei_Schreiben(double[][] Array,String Dateiname)
+    {
         try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter("Bubblesort.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(Dateiname));
             writer.write(Integer.toString(stepos));
             writer.newLine();
-            for(int i = 0; i < BubbleSave.length; i += stepos)
+            for(int i = 0; i < Array.length; i += stepos)
             {                
                 writer.write(Integer.toString(i));
                 writer.newLine();
-                writer.write(Double.toString(BubbleSave[i][0]));
-                writer.newLine();   
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter("Insertsort.txt"));
-            writer.write(Integer.toString(stepos));
-            writer.newLine();
-            for(int i = 0; i < InsertSave.length; i += stepos)
-            {                
-                writer.write(Integer.toString(i));
-                writer.newLine();
-                writer.write(Double.toString(InsertSave[i][0]));               
-                writer.newLine();   
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter("Mergesort.txt"));
-            writer.write(Integer.toString(stepos));
-            writer.newLine();
-            for(int i = 0; i < MergeSave.length; i += stepos)
-            {                
-                writer.write(Integer.toString(i));
-                writer.newLine();
-                writer.write(Double.toString(MergeSave[i][0]));
+                writer.write(Double.toString(Array[i][0]));
                 writer.newLine();   
             }
             writer.close();
@@ -245,77 +209,51 @@ public class Grafik extends Ereignisanwendung
             e.printStackTrace();
         }
     }
-
 
     public void Read_Array() {
+        BubbleSave = Aus_Datei_Ablesen("BubbleSort.txt");
+        InsertSave = Aus_Datei_Ablesen("InsertSort.txt");
+        MergeSave = Aus_Datei_Ablesen("MergeSort.txt");
+    }
+
+    public double[][] Aus_Datei_Ablesen(String Dateiname)
+    {
+        double[][] Array = new double[1200][1];
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("Bubblesort.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(Dateiname));
 
             stepos = Integer.parseInt(reader.readLine());
 
             // Initialisiere BubbleSave mit einer passenden Größe (Beispielwerte!)
-            BubbleSave = new double[1200][1200]; // Falls nötig, Größe anpassen
             String line;
 
             while ((line = reader.readLine()) != null)
             {
                 int x = Integer.parseInt(line);
-                BubbleSave[x][0] = Double.parseDouble(reader.readLine());
+                Array[x][0] = Double.parseDouble(reader.readLine());
             }
 
             reader.close();
+            return Array;
         } catch (IOException e) {
             e.printStackTrace();
         } catch (NumberFormatException e) {
             System.out.println("Fehler beim Parsen einer Zahl.");
             e.printStackTrace();
+            return Array;
         }
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("InsertSort.txt"));
+        return Array;
+    }
 
-            stepos = Integer.parseInt(reader.readLine());
+    public void Update_Info_Etikett(double time, int durchläufe, int current)
+    {
+        int remaining = durchläufe - current;
+        double total = time*remaining;
 
-            // Initialisiere BubbleSave mit einer passenden Größe (Beispielwerte!)
-            InsertSave = new double[1200][1200]; // Falls nötig, Größe anpassen
-            String line;
-
-            while ((line = reader.readLine()) != null)
-            {
-                int x = Integer.parseInt(line);
-                InsertSave[x][0] = Double.parseDouble(reader.readLine());
-            }
-
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NumberFormatException e) {
-            System.out.println("Fehler beim Parsen einer Zahl.");
-            e.printStackTrace();
-        }
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("MergeSort.txt"));
-
-            stepos = Integer.parseInt(reader.readLine());
-
-            // Initialisiere BubbleSave mit einer passenden Größe (Beispielwerte!)
-            MergeSave = new double[1200][1200]; // Falls nötig, Größe anpassen
-            String line;
-
-            while ((line = reader.readLine()) != null)
-            {
-                int x = Integer.parseInt(line);
-                MergeSave[x][0] = Double.parseDouble(reader.readLine());
-            }
-
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NumberFormatException e) {
-            System.out.println("Fehler beim Parsen einer Zahl.");
-            e.printStackTrace();
-        }
     }
 }
+
+
 
 
 
